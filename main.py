d1c2e6d0c3efb48e0789a2c83b581edd1fa8ea9a
Chris Brown <cabrown19@gmail.com>
2014-06-20 13:45:19 -0700

    changed the app to use angular instead of jinja

--- main.py
+++ main.py
@@ -15,31 +15,13 @@
 # limitations under the License.
 #
 import webapp2
-import os       ## For getting the current directory
-import jinja2   ## for templating html
 
 from google.appengine.ext import db     ## import dataStore databases
 
-## Formatting the jinja environment. All .html files must stored in a "htmlFiles" folder
-template_dir = os.path.join(os.path.dirname(__file__), 'htmlFiles')
-jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
-
-## A class for rendering with jinja templates
-class Handler(webapp2.RequestHandler):
-    def write(self, *a, **kw):
-        self.response.write(*a, **kw)
-
-    def render_str(self, template, **params):
-        t = jinja_env.get_template(template)
-        return t.render(params)
-
-    def render(self, template, **kw):
-        self.write(self.render_str(template, **kw))
-
-class MainHandler(Handler):
+class MainHandler(webapp2.RequestHandler):
     def get(self):
+        self.response.write("")
         ## Add more arguments here for additional inputs to the template
-        self.render("initialHtml.html")
 
 ## Need to add other handlers for new pages.
 app = webapp2.WSGIApplication([
@@ -47,3 +29,12 @@ app = webapp2.WSGIApplication([
     ##example (need a comma on previous line):
 ##  ('/newpage', NewPageHandler)  ## Now you need a NewPageHandler like the MainHandler
 ], debug=True)
+
+
+
+
+
+
+
+
+
